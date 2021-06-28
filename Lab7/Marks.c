a015ecdd6962782c405ee576cdb560d9e69a5b09
Ronit Bangard <83938931+Ronit27@users.noreply.github.com>
2021-06-28 11:45:36 +0530

    Add files via upload

---
+++ Lab7/Marks.c
@@ -0,0 +1,25 @@
+//Demonstrate reading a two-dimensional array of marks which stores marks of 4 students in 3 subjects and display the highest marks in each subject.
+#include<stdio.h>
+int main()
+{
+	int marks[4][3],max,i,j;
+	printf("Enter Marks of four students in three subjects\n");
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<3;j++)
+		scanf("%d",&marks[i][j]);
+	}
+	for(j=0;j<3;j++)
+	{
+		max=marks[0][j];
+		for(i=1;i<4;i++)
+		{
+			if(max<marks[i][j])
+			{
+				max=marks[i][j];
+			}
+		}
+		printf("Highest marks in Subject %d = %d \n",j+1,max);
+	}
+	return 0;
+}
